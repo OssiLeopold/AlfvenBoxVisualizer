6e299cb8bed96ffa2570835446362e29a146077d
rxelmer <elme@live.fi>
2025-06-10 13:14:15 +0300

    make .gitignore work

--- test.py
+++ test.py
@@ -3,3 +3,4 @@ bulkpath = "/home/rxelmer/Documents/turso/bulks/sim15/"
 vlsvobj = pt.vlsvfile.VlsvReader(bulkpath + "bulk.0000010.vlsv")
 rho = vlsvobj.read_variable("proton/vg_rho")
 print(min(rho)/1e6)
+print("hello")
