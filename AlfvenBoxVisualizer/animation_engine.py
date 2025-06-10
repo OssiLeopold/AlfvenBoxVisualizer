07700586d1942401f08b120984ac23fa801bce6d
rxelmer <elme@live.fi>
2025-06-10 09:39:43 +0300

    add 3D functionality

--- AlfvenBoxVisualizer/animation_engine.py
+++ AlfvenBoxVisualizer/animation_engine.py
@@ -1,7 +1,10 @@
+import os
 import analysator as pt
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
+os.environ['PATH']='/home/rxelmer/Documents/turso/appl/tex-basic/texlive/2023/bin/x86_64-linux:'+ os.environ['PATH'] #enabling use of latex
+os.environ['PTNOLATEX']='1' #enabling use of latex
 
 R_E = 6371e3 # Earth radius
 COLORS = ["blue", "orange"]
@@ -42,8 +45,8 @@ class AnimationEngine:
             Min = 0
             Max = 1.5 * max(vlsvobj.read_variable(object.variable,operator="y")) / object.unit
         else:
-            Min = min(vlsvobj.read_variable(vlsvobj.variable, operator=object.component)) / object.unit
-            Max = max(vlsvobj.read_variable(vlsvobj.variable, operator=object.component)) / object.unit
+            Min = min(vlsvobj.read_variable(object.variable, operator=object.component)) / object.unit
+            Max = max(vlsvobj.read_variable(object.variable, operator=object.component)) / object.unit
 
         levels = 100                                                      
         level_boundaries = np.linspace(Min, Max, levels + 1)
@@ -66,22 +69,23 @@ class AnimationEngine:
 
         cbar = plt.colorbar(p[0], cmap = "hot")
         if object.variable == "vg_b_vol":
-            cbar.colorbar(f"B_{object.component} [nT]")
+            cbar.set_label(f"B_{object.component} [nT]")
         elif object.variable == "proton/vg_v":
-            cbar.set_zlabel(f"v_{object.component} [km/s]")
+            cbar.set_label(f"v_{object.component} [km/s]")
 
-        p[0].set_xlabel("x [RE]")
-        p[0].set_ylabel("y [RE]")
+        ax.set_xlabel("x [RE]")
+        ax.set_ylabel("y [RE]")
         self.timelabel = ax.text(self.xmax, self.ymax*1.01, "")
 
         anim = animation.FuncAnimation(fig, self.update_2D, frames = object.bulkfile_n + 1, interval = 20)
 
         writer = animation.PillowWriter(fps=5)
-        anim.save(animation.name, writer = writer)
+        anim.save(object.name, writer = writer)
         plt.close()
 
     def update_2D(self, frame):
-        self.p.remove()
+        self.p[0].remove()
+        object = self.object
         fname = f"bulk.{str(frame).zfill(7)}.vlsv"
         vlsvobj = pt.vlsvfile.VlsvReader(object.bulkpath + fname)   
 
@@ -101,3 +105,70 @@ class AnimationEngine:
 
         return self.p
 
+    def animation_3D(self):
+        vlsvobj = self.vlsvobj
+        object = self.object
+        cid_sort = self.cid_sort
+
+        fig = plt.figure()
+        ax = fig.add_subplot(projection = '3d')
+
+        if object.component == "total":
+            Min = 0
+            Max = 1.5 * max(vlsvobj.read_variable(object.variable,operator="y")) / object.unit
+        else:
+            Min = min(vlsvobj.read_variable(object.variable, operator=object.component)) / object.unit
+            Max = max(vlsvobj.read_variable(object.variable, operator=object.component)) / object.unit
+
+        p = []
+        if object.component == "total":
+            y = np.array(vlsvobj.read_variable(object.variable, operator="y")[cid_sort]) / object.unit
+            z = np.array(vlsvobj.read_variable(object.variable, operator="z")[cid_sort]) / object.unit
+            magnitude = np.sqrt(y**2 + z**2)
+            magnitude_mesh = magnitude.reshape(-1, self.x_length)
+            p.append(ax.plot_surface(self.x_mesh, self.y_mesh, magnitude_mesh, color=COLORS[0]))
+        else:
+            value = np.array(vlsvobj.read_variable(object.variable, operator=object.component)[cid_sort]) / object.unit
+            value_mesh = value.reshape(-1, self.x_length)
+            p.append(ax.plot_surface(self.x_mesh, self.y_mesh, value_mesh, color = COLORS[0]))
+        
+        self.p = p
+        self.ax = ax
+
+        if object.variable == "vg_b_vol":
+            ax.set_zlabel(f"B_{object.component} [nT]")
+        elif object.variable == "proton/vg_v":
+            ax.set_zlabel(f"v_{object.component} [km/s]")
+        ax.set_zlim(Min, Max)
+
+        ax.set_xlabel("x [RE]")
+        ax.set_ylabel("y [RE]")
+        self.timelabel = ax.text(self.xmax, self.ymax*1.01, Max, "")
+
+        anim = animation.FuncAnimation(fig, self.update_3D, frames = object.bulkfile_n + 1, interval = 20)
+
+        writer = animation.PillowWriter(fps=5)
+        anim.save(object.name, writer = writer)
+        plt.close()
+
+    def update_3D(self, frame):
+        self.p[0].remove()
+        object = self.object
+        fname = f"bulk.{str(frame).zfill(7)}.vlsv"
+        vlsvobj = pt.vlsvfile.VlsvReader(object.bulkpath + fname)   
+
+        time = vlsvobj.read_parameter("time")
+        self.timelabel.set_text(f"{time:.1f}s")
+        
+        if object.component == "total":
+            y = np.array(vlsvobj.read_variable(object.variable, operator="y")[self.cid_sort]) / object.unit
+            z = np.array(vlsvobj.read_variable(object.variable, operator="z")[self.cid_sort]) / object.unit
+            magnitude = np.sqrt(y**2 + z**2)
+            magnitude_mesh = magnitude.reshape(-1, self.x_length)
+            self.p[0] = self.ax.plot_surface(self.x_mesh, self.y_mesh, magnitude_mesh, color = COLORS[0])
+        else:
+            value = np.array(vlsvobj.read_variable(object.variable, operator=object.component)[self.cid_sort]) / object.unit
+            value_mesh = value.reshape(-1, self.x_length)
+            self.p[0] = self.ax.plot_surface(self.x_mesh, self.y_mesh, value_mesh, color = COLORS[0])
+
+        return self.p
--- AlfvenBoxVisualizer/main.py
+++ AlfvenBoxVisualizer/main.py
@@ -9,15 +9,16 @@ bulkpath = "/home/rxelmer/Documents/turso/bulks/sim15/"
 # Enter number of frames to be animated, define as None for all files
 bulkfile_number = 10
 
-animations = [("2D", "B_tot","anim1.gif"),("2D", "v_y","anim2.gif")]
+animations = [("3D", "B_tot","anim1.gif"),("3D", "v_y","anim2.gif")]
 
-for object in animations:
-    object = AnimationSpecs(
-            object[0], object[1], object[2], bulkpath, bulkfile_number)
+for i, object in enumerate(animations):
+    animations[i] = AnimationSpecs(object[0], object[1], object[2], bulkpath, bulkfile_number)
 
-def main():
-    with mp.Pool(len(animations)) as process:
-        process.map(AnimationEngine, animations)
 
-if __name__ == "__main__":
-    main()
+with mp.Pool(len(animations)) as process:
+    process.map(AnimationEngine, animations)
+
+#if __name__ == "__main__":
+#    animations = [("2D", "B_tot","anim1.gif"),("2D", "v_y","anim2.gif")]
+#    forma
+#    main()
