cf3a9a9f437f540b5ce511eebb6061f32766e774
rxelmer <elme@live.fi>
2025-06-10 13:16:33 +0300

    make .gitignore work

--- test.py
+++
@@ -1,6 +0,0 @@
-import analysator as pt
-bulkpath = "/home/rxelmer/Documents/turso/bulks/sim15/"
-vlsvobj = pt.vlsvfile.VlsvReader(bulkpath + "bulk.0000010.vlsv")
-rho = vlsvobj.read_variable("proton/vg_rho")
-print(min(rho)/1e6)
-print("hello")
